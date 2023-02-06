489c54b11d644dbccc41fba1a6d5fa418b19d960
Kosta <dukov423@mail.ru>
2023-02-06 21:30:10 +0500

    fix test

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -10,15 +10,14 @@ public class PostmanEchoTest {
         // Given - When - Then
         given()
                 .baseUri("https://postman-echo.com")
-                .contentType("text/plain; charset=UTF-8")
-                .body("Пасхалка для ревьювера. А как посмотреть в IDEA сам респонс ответ? Пришлось смотреть через постман, т.к тут не нашёл где ")
+                .body("test")
 
                 .when()
                 .post("/post")
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Пасхалка для ревьювера. А как посмотреть в IDEA сам респонс ответ? Пришлось смотреть через постман, т.к тут не нашёл где "))
+                .body("data", equalTo("test"))
         ;
     }
 }
